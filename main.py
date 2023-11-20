7c35cb72e7b428b263f773571be5f65ad25864f7
sankhapanineog <102051577+sankhapanineog@users.noreply.github.com>
2023-11-20 20:31:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,20 @@ def main():
 
     # Explain the advantages of AI-based asset health forecasting
     st.subheader("Advantages of AI-based Asset Health Forecasting:")
-    
+    st.write("1. *Early Detection:* AI models can detect subtle patterns indicative of asset degradation before"
+             " visible signs appear, allowing for early intervention and maintenance.")
+
+    st.write("2. *Data-Driven Insights:* AI algorithms analyze large datasets, providing data-driven insights into"
+             " asset performance and health based on historical patterns and real-time data.")
+
+    st.write("3. *Predictive Maintenance:* AI can predict when equipment is likely to fail, enabling proactive"
+             " maintenance schedules and minimizing downtime.")
+
+    st.write("4. *Cost Savings:* Predictive maintenance and early detection of issues lead to cost savings by"
+             " preventing major breakdowns and reducing unplanned downtime.")
+
+    st.write("5. *Continuous Improvement:* AI models can be continually trained and improved with new data, adapting"
+             " to changing conditions and improving accuracy over time.")
 
 if _name_ == "_main_":
     main()
